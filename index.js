47bb570ce2b7772b3ff7a90761f5ad5e49a3afd2
Sahiladiv <sahiladivarekar17@gmail.com>
2020-11-18 23:50:54 +0530

    New changes made in index files

--- index.js
+++ index.js
@@ -1,3 +1,72 @@
+document.getElementById('searchButton').addEventListener('click',function(){
+  var searchContent = document.getElementById('search').value;
+  var contents = document.getElementsByClassName('itemName');
+  for (var i = 0; i < contents.length; i++) {
+    if(searchContent == contents[i].textContent){
+      document.getElementById('bigContainer').style.display="none";
+      create(i)
+      break;
+    }
+  }
+});
+
+function create(i){
+
+  var searchDiv = document.createElement("div");
+  searchId = document.createAttribute("id");
+  searchId.value = "searchedContainer"
+  searchDiv.setAttributeNode(searchId)
+
+
+  // var searchingContainer = document.getElementById('searchedContainer');
+  // searchingContainer.style.display="block";
+  var backButton = document.createElement("button");
+  var backText = document.createTextNode("Back");
+
+  var backId = document.createAttribute("id");
+  backId.value="back"
+  backButton.setAttributeNode(backId)
+  backButton.appendChild(backText);
+  backButton.className="myBackButton";
+
+  backClick = document.createAttribute("onclick");
+  backClick.value="back()";
+  backButton.setAttributeNode(backClick)
+
+
+  searchDiv.appendChild(backButton);
+
+    var itemContents = document.getElementsByClassName('contain')[i];
+    var cloneContents = itemContents.cloneNode(true);
+    cloneContents.className = "myContain"
+    searchDiv.appendChild(cloneContents);
+    document.body.appendChild(searchDiv)
+
+}
+
+function back(){
+
+  var t = document.getElementById('searchedContainer');
+  t.remove();
+    document.getElementById('bigContainer').style.display="grid";
+
+document.getElementById('searchedContainer').style.display="none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var cartContent = document.getElementsByClassName('contain');
 var itemsAdded = [];
 var index;
